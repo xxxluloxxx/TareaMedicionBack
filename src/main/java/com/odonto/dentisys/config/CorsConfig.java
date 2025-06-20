4028f7ccbe94dbad10220d91a79e5b228a827821
Paul Arias <paul.arias@bayteq.com>
2025-06-20 08:15:28 -0500

    Se agregan cors

--- src/main/java/com/odonto/dentisys/config/CorsConfig.java
+++ src/main/java/com/odonto/dentisys/config/CorsConfig.java
@@ -13,6 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "https://jade-tulumba-fd348a.netlify.app",
                         "https://dentisysfront-production.up.railway.app",
                         "http://localhost:5173",
+                        "http://93.127.217.21:5173",
                         "http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
